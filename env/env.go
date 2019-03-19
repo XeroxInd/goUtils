5775196e9ca8575a3772ab3586288dcc11cd8034
bvigneron <bastien@libertymedical.fr>
2019-03-19 17:10:24 +0100

    Add Typed func for env var parsing.

--- env/env.go
+++ env/env.go
@@ -1,6 +1,9 @@
 package env
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 // GetEnvOrElse return the current value of env variable
 // or defaultValue
@@ -9,6 +12,48 @@ func GetEnvOrElse(env string, defaultValue string) string {
 	switch ok {
 	case true:
 		return v
+	default:
+		return defaultValue
+	}
+}
+
+func GetIntEnvOrElse(env string, defaultValue int) int {
+	v, ok := os.LookupEnv(env)
+	switch ok {
+	case true:
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			return defaultValue
+		}
+		return val
+	default:
+		return defaultValue
+	}
+}
+
+func GetFloatEnvOrElse(env string, defaultValue float64) float64 {
+	v, ok := os.LookupEnv(env)
+	switch ok {
+	case true:
+		val, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return defaultValue
+		}
+		return val
+	default:
+		return defaultValue
+	}
+}
+
+func GetBoolEnvOrElse(env string, defaultValue bool) bool {
+	v, ok := os.LookupEnv(env)
+	switch ok {
+	case true:
+		val, err := strconv.ParseBool(v)
+		if err != nil {
+			return defaultValue
+		}
+		return val
 	default:
 		return defaultValue
 	}
