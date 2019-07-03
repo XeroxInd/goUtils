0a9a2beb9b180dc98a7c12b5ff8525b810ba1232
Céline Leduc <celine@libertymedical.fr>
2019-07-03 15:37:46 +0200

    Add library for nats

---
+++ nats/nats.go
@@ -0,0 +1,63 @@
+package nats
+
+import (
+	"fmt"
+	"github.com/nats-io/go-nats-streaming"
+	"log"
+	"os"
+)
+
+func (c *Config) Load() {
+	var err error
+	if c.clusterID == "" {
+		c.clusterID = os.Getenv("NATS_CLUSTER_ID", )
+	}
+	if c.clientID == "" {
+		c.clientID = os.Getenv("NATS_CLIENT_ID", )
+	}
+	if c.url == "" {
+		c.url = os.Getenv("NATS_URL")
+	}
+	if len(c.clientID) == 0 || len(c.clusterID) == 0 || len(c.url) == 0 {
+		log.Fatal("Some env variable aren't defined, please check it, NATS_CLUSTER_ID, NATS_CLIENT_ID and NATS_URL must be set")
+	}
+	c.connection, err = stan.Connect(
+		c.clusterID,
+		c.clientID,
+		stan.NatsURL(c.url),
+		stan.SetConnectionLostHandler(func(_ stan.Conn, reason error) {
+			log.Fatalf("connection to NATS server lost, reason: %v\nThen I quit: \"Au revoir...\"", reason)
+		}))
+	if err != nil {
+		log.Fatalf("unable to connect to NATS Streaming server (%s): %s", c.url, err.Error())
+	}
+	log.Printf("connected to NATS Streaming server (%s), with clientID : %s\n", c.url, c.clientID)
+}
+
+func (c *Config) Subscribe(subject string, handler stan.MsgHandler, opts ...stan.SubscriptionOption) (sub stan.Subscription, err error) {
+	log.Printf("Subscribing to subject '%s'\n", subject)
+	sub, err = c.connection.Subscribe(subject, handler, opts...)
+	if err != nil {
+		log.Print(fmt.Errorf("unable to subscribe: %s", err.Error()), "nats")
+	}
+	return
+}
+
+func (c *Config) Publish(subject string, message []byte) (err error) {
+	return c.connection.Publish(subject, message)
+}
+
+func (c *Config) ShutDown(subs ...stan.Subscription) (err error){
+	log.Print("closing NATS connection...")
+	for _, s := range subs {
+		err = s.Close()
+		if err != nil {
+			log.Print(fmt.Errorf("error when close NATS subscription: %s", err.Error()), "nats")
+		}
+	}
+	err = c.connection.Close()
+	if err != nil {
+		log.Print(fmt.Errorf("unable to close NATS connection : %s", err.Error()), "nats")
+	}
+	return
+}
---
+++ nats/nats_test.go
@@ -0,0 +1,50 @@
+package nats
+
+import (
+	stan "github.com/nats-io/go-nats-streaming"
+	"log"
+	"testing"
+)
+
+var c Config
+var channel = "test"
+var sub stan.Subscription
+
+func init() {
+	c = Config{
+		clusterID: "libmed",
+		clientID:  "test",
+		url:       "nats://nats.qa.svc.cluster.local:4222",
+	}
+	c.Load()
+}
+
+func TestSubscribe(t *testing.T) {
+	var err error
+	sub, err = c.Subscribe(channel, func(m *stan.Msg) {
+		log.Printf("Received a message: %s\n", string(m.Data))
+	})
+	if err != nil {
+		t.Errorf("must not return an error, %s", err.Error())
+	} else {
+		t.Logf("ok")
+	}
+}
+
+func TestPublish(t *testing.T) {
+	err := c.Publish(channel, []byte("Hello World"))
+	if err != nil {
+		t.Errorf("must not return an error, %s", err.Error())
+	} else {
+		t.Logf("ok")
+	}
+}
+
+func TestClose(t *testing.T) {
+	err := c.ShutDown(sub)
+	if err != nil {
+		t.Errorf("must not return an error, %s", err.Error())
+	} else {
+		t.Logf("ok")
+	}
+}
